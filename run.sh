3590856d336831e3bdb6cfd1ab7d773c8bb9cf94
askbk <askbk@protonmail.com>
2020-03-10 13:47:43 +0100

    Better handling of data dir

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
-cd COVID-19
-git pull
-cd ..
+FILE=COVID-19
+
+if [ -d "$FILE" ]; then
+    cd COVID-19
+    git pull
+    cd ..
+else
+    git clone https://github.com/CSSEGISandData/COVID-19.git
+fi
+
 python analysis.py
