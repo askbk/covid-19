a6929d582384f0562b52bc0872ffa0e7b2598847
askbk <askbk@protonmail.com>
2020-03-21 12:13:07 +0100

    Try to fit growth curve to logistic function.

--- analysis.py
+++ analysis.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas
 import datetime
-
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from scipy.optimize import curve_fit
 
 
 def find_inflection(time_series, delta):
@@ -61,7 +63,9 @@ def aggregate_by_row_label(data):
         label = row[1]
         row_data = np.array([row[i] for i in range(4, len(row))])
         
-        aggregate += row_data
+        # only aggregate data outside china
+        if not label == "China":
+            aggregate += row_data
 
         if label in labels:
             labels[label] += row_data
@@ -71,7 +75,13 @@ def aggregate_by_row_label(data):
     return labels, aggregate[0]
 
 
-start_date = datetime.datetime(2020, 1, 22)
+def logistic_growth(t, L, k, t_0):
+    return L / (1 + np.exp(-k*(t-t_0)))
+
+def logistic_growth_d(t, L, k, t_0):
+    return (L * k * np.exp(-k * (t - t_0))) / np.exp(-k * (t - t_0))**2
+
+start_date = datetime.date(2020, 1, 22)
 
 loc = 'COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
 covid_data = pandas.read_csv(loc).values
@@ -81,20 +91,50 @@ country_data, world_data = aggregate_by_row_label(covid_data)
 
 def print_all():
     found = list()
-    not_found = list()
     for country, data in country_data.items():
         end, has_end = find_infection_end(data, start_date)
         if has_end:
             found.append(f'{country}\t\t{end}')
-        else:
-            not_found.append(country)
     
     print('Countries with end dates:')
     for country in found:
         print(country)
 
-    print('Countries without end dates:', not_found)
+    end, has_end = find_infection_end(world_data, start_date)
+
+    print(f'World status: {end}')
+
+    usa_data = country_data['US']
+    date_range = mdates.drange(start_date, datetime.date.today(), datetime.timedelta(days=1))
+    
+    #plt.plot_date(date_range, usa_data)
+
+    #initial guess for curve fit coefficients
+    guess = [300000, 0.1, 50]
+    # coefficients and curve fit for curve
+    xdata = np.arange(0, len(usa_data))
+    popt, pcov = curve_fit(logistic_growth, xdata, usa_data, p0=guess)
+
+    
+    #xfuture = np.arange(0, 365)
+
+    # Growth
+    plt.plot(xdata, logistic_growth(xdata, *popt))
+    plt.plot(xdata, usa_data)
+        
+    # derivatives
+    plt.plot(xdata, logistic_growth_d(xdata, *popt))
+    plt.plot(xdata[:-1], np.diff(usa_data))
+    
+    
+    # predictions
+    #plt.plot(xfuture, logistic_growth(xfuture, *popt))
+    #plt.plot(xfuture, logistic_growth_d(xfuture, *popt))
+
+    plt.show()
+
+    #plt.plot_date(date_range, v_fit)
+    #plt.show()
+
 
 print_all()
-
-
